0a58e014980a1a613e490d62ade9c4e133a03f70
Bea P. Santana <beapsant@gmail.com>
2020-09-10 18:40:28 +0100

    Improve MenuItem styles

--- src/components/MenuItem/index.js
+++ src/components/MenuItem/index.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import { withRouter } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import './styles.scss';
 
 function MenuItem(props) {
-  const { title, imageUrl, size, linkUrl, history, match } = props;
+  const { title, imageUrl, size, linkUrl } = props;
   const style = {
     backgroundImage: `url(${imageUrl})`
   };
 
   return (
-    <div
-      className={`menu-item ${size}`}
-      onClick={() => history.push(`${match.url}${linkUrl}`)}
-    >
-      <div style={style} className="menu-item__image"></div>
-      <div className="menu-item__content">
-        <h1 className="menu-item__title">{title}</h1>
-        <span className="menu-item__subtitle">Shop now</span>
+    <Link to={linkUrl} className="links-menu-items">
+      <div className={`menu-item ${size}`}>
+        <div style={style} className="menu-item__image"></div>
+        <div className="menu-item__content">
+          <h1 className="menu-item__title">{title}</h1>
+          <span className="menu-item__subtitle">Shop now</span>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
-export default withRouter(MenuItem);
+export default MenuItem;
