222da8c217ce5002f4c76cc438050ebec3a8cf0c
Bea P. Santana <beapsant@gmail.com>
2020-09-10 16:25:33 +0100

    Style MenuItem

--- src/components/MenuItem/index.js
+++ src/components/MenuItem/index.js
@@ -9,12 +9,19 @@ function MenuItem(props) {
   };
 
   return (
-    <div style={style} className={`menu-item ${size}`}>
+    <div className={`menu-item ${size}`}>
+      <div style={style} className="menu-item__image"></div>
       <div className="menu-item__content">
         <h1 className="menu-item__title">{title}</h1>
-        <span className="menu-item__subtitle">SHOP NOW</span>
+        <span className="menu-item__subtitle">Shop now</span>
       </div>
     </div>
+    // <div style={style} className={`menu-item ${size}`}>
+    //   <div className="menu-item__content">
+    //     <h1 className="menu-item__title">{title}</h1>
+    //     <span className="menu-item__subtitle">SHOP NOW</span>
+    //   </div>
+    // </div>
   );
 }
 
