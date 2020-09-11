a0e0f4bb4cd9ef850a47e3cdae4ca4218cdfe0fe
Bea P. Santana <beapsant@gmail.com>
2020-09-11 18:38:01 +0100

    Add user data to App component

--- src/App.js
+++ src/App.js
@@ -7,18 +7,42 @@ import Home from './pages/Home';
 import Shop from './pages/Shop';
 import SignInUp from './pages/SignInUp';
 import Header from './components/Header';
+import { auth } from './firebase/firebase.utils';
 
-function App() {
-  return (
-    <Router>
-      <Header />
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/shop" component={Shop} />
-        <Route path="/signin" component={SignInUp} />
-      </Switch>
-    </Router>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    };
+  }
+
+  unsubcribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubcribeFromAuth = auth.onAuthStateChanged((user) => {
+      this.setState({ currentUser: user });
+      console.log(user);
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubcribeFromAuth();
+  }
+
+  render() {
+    return (
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/shop" component={Shop} />
+          <Route path="/signin" component={SignInUp} />
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
