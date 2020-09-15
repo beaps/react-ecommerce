5fb1be9b42c4c12016d4ce12a5279d0e1591135a
Bea P. Santana <beapsant@gmail.com>
2020-09-15 17:10:26 +0100

    Change StripeCheckoutButton position

--- src/pages/Checkout/index.js
+++ src/pages/Checkout/index.js
@@ -38,12 +38,12 @@ function Checkout(props) {
       <div className="checkout__total">
         <span>TOTAL: €{total}</span>
       </div>
+      <StripeCheckoutButton price={total} />
       <div className="checkout__text-warning">
         <p>*Please use the following test credit card for payments*</p>
         <p>4242 4242 4242 4242 - *Exp: 09/20 - CVV: 123</p>
         <p>*Exp: current month / current year</p>
       </div>
-      <StripeCheckoutButton price={total} />
     </div>
   );
 }
